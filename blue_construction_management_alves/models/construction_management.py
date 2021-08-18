ff379a9b114509ad259a4e09bbc74b59c7188050
Diego Santos <69399665+blue-connect@users.noreply.github.com>
2021-08-18 13:44:46 -0300

    Alterações feitas

--- blue_construction_management_alves/models/construction_management.py
+++ blue_construction_management_alves/models/construction_management.py
@@ -54,12 +54,11 @@ class material_consume(models.Model):
     uom_id = fields.Many2one('uom.uom', 'Unit of Measure')
     task_id = fields.Many2one('project.task', 'Task')
     price_unit = fields.Float('Valor Unitario',
-                              digits=dp.get_precision('Product Price'))
-    # , default=0.0)
-    price_subtotal = fields.Float('Valor Total', compute='_compute_price',
-                                  digits=dp.get_precision('Product Price'))
+                              digits=dp.get_precision('Product Price'), default=0.0)
 
-    # , default=0.0)
+    price_subtotal = fields.Float('Valor Total', compute='_compute_price',
+                                  store=True,
+                                  digits=dp.get_precision('Product Price'), default=0.0)
 
     @api.depends('price_unit', 'product_qty', 'product_id')
     def _compute_price(self):
@@ -97,17 +96,16 @@ class project_task(models.Model):
         required=False)
 
     price_total_materiais = fields.Float(string='Custo com Materias Utilizados',
-                                         #                                         compute='_calculate_price_total_materiais',
-                                         #                                        store=True,
+                                         compute='_calculate_price_total_materiais',
+                                         store=True,
                                          help="Valor total dos materias utilizados na OS atual")
 
-
-#    def _calculate_price_total_materiais(self):
-#        for pt in self:
-#            ptsumqty = 0
-#            for line in pt.consume_material_ids:
-#                ptsumqty += line.price_subtotal
-#        pt.price_total_materiais = ptsumqty
+    def _calculate_price_total_materiais(self):
+        for pt in self:
+            ptsumqty = 0
+            for line in pt.consume_material_ids:
+                ptsumqty += line.price_subtotal
+        pt.price_total_materiais = ptsumqty
 
 
 #   def _calculate_qtd_materiais(self):
