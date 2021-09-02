42dcbc82fbf0baabe9d97c1aa0f7d6d504e54444
Diego Santos <69399665+blue-connect@users.noreply.github.com>
2021-09-02 17:23:57 -0300

    a

--- teste/post_segline2.py
+++ teste/post_segline2.py
@@ -3,38 +3,29 @@ import json
 import jsonpickle
 import io
 import pickle
+
 try:
     to_unicode = unicode
 except NameError:
     to_unicode = str
 
-url = "http://sip.segline.srv.br/jeta/api/campanha/cliente/importado"
+url = "http://viacep.com.br/ws/42850000/json/"
 
-payload = json.dumps({
-    "CAMPANHA": 6374,
-    "CAMPO": "CPFCNPJ",
-    "TABELA": "CAMPANHA_IMPORTACAO",
-    "EMPRESA": "new",
-    "JETA": 38427,
-    "PAGESIZE": 2,
-    "PAGEINDEX": 2
-})
-headers = {
-    'Content-Type': 'application/json'
-}
+payload = {}
+headers = {}
 
 
-def buscar_dados_cpfcnpj(cpfcnpj):
-    response = requests.request("POST", url, headers=headers, data=payload)
+def buscar_dados_localidade():
+    response = requests.request("GET", url, headers=headers, data=payload)
     dados = response.json()
     todos = json.loads(response.content)
     #    print(todos)
-    print(todos['result'])
+    print(todos)
 
 
 if url:
     # buscar_dados()
-    buscar_dados_cpfcnpj("00001150537")
+    buscar_dados_localidade()
 
 '''
 # Write JSON file
@@ -57,14 +48,14 @@ print(data_loaded)
 '''
 with open('data.json', 'wb') as fp:
     pickle.dump(dados, fp)'''
-#with open('/mnt/c/Users/Kerberos/Documents/GitHub/blueapps/teste/data.txt', 'w') as f:
+# with open('/mnt/c/Users/Kerberos/Documents/GitHub/blueapps/teste/data.txt', 'w') as f:
 #  json.dump(response, f, ensure_ascii=False)
-#print(response.json())
-#with open('/mnt/c/Users/Kerberos/Documents/GitHub/blueapps/teste/data.json', 'w') as f:
+# print(response.json())
+# with open('/mnt/c/Users/Kerberos/Documents/GitHub/blueapps/teste/data.json', 'w') as f:
 #    json.dump(response, f, ensure_ascii=False, indent=4)
 
-#with open('data.txt', 'w') as f:
+# with open('data.txt', 'w') as f:
 #  json.dump(data, f, ensure_ascii=False)
 
-#rate = jsonpickle.encode(response)
-#print(ret)
+# rate = jsonpickle.encode(response)
+# print(ret)
